831d42421c708438857c743c40e4fb8314b83554
Ahmed Galal <31260519+Ahmed-Galal10@users.noreply.github.com>
2022-10-10 01:37:44 +0200

    Time: 11 ms (11.19%), Space: 45.5 MB (20.66%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
@@ -1,10 +1,6 @@
 class Solution {
     public int mostWordsFound(String[] sentences) {
-        int maxWords = 0;
-
-        for (String sentence : sentences){
-            maxWords = Math.max(maxWords, sentence.split(" ").length);
-        }
-        return maxWords;
+        return Stream.of(sentences).mapToInt( sentence -> sentence.trim().split(" ").length)
+                                    .max().getAsInt();
     }
 }
