948bc2ada9f01aa757bf9775c3714c4643810626
Ahmed Galal <31260519+Ahmed-Galal10@users.noreply.github.com>
2022-10-11 01:44:13 +0200

    Time: 356 ms (78.59%), Space: 0B (100.00%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -3,9 +3,9 @@ BEGIN
   DECLARE M INT;
   SET M = N - 1;
   RETURN (
-      SELECT DISTINCT salary
-      FROM employee
-      ORDER BY salary DESC
-      LIMIT M, 1
+      SELECT IFNULL(NULL, (SELECT DISTINCT salary
+                           FROM employee
+                           ORDER BY salary DESC
+                           LIMIT M, 1))
   );
 END
