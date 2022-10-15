b6cf1e9ff9128834c46c20f61bdfa29c9f69f6ed
Ahmed Galal <31260519+Ahmed-Galal10@users.noreply.github.com>
2022-10-15 19:18:17 +0200

    Time: 3 ms (34.78%), Space: 44.8 MB (45.03%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.java
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -4,23 +4,12 @@ import java.util.stream.Collectors;
 
 
 class Solution {
-    public String restoreString(String originalString, int[] indices) {
-        SortedMap<Integer, String> sortedIndicesWithChars = matchingIndicesWithStrings(originalString, indices);
-        
-        return concateMapValues(sortedIndicesWithChars);
-        
-    }
-
-    static SortedMap<Integer, String> matchingIndicesWithStrings(String originalString, int[] indices){
-        SortedMap<Integer, String> sortedIndicesWithChars = new TreeMap<>();
-
-        for (int c = 0; c < originalString.length(); c++) {
-            sortedIndicesWithChars.put(indices[c], String.valueOf(originalString.charAt(c)));
+    public String restoreString(String s, int[] indices) {
+        StringBuilder originalString = new StringBuilder(s);
+        for (int i = 0; i < indices.length; i++) {
+            originalString.setCharAt(indices[i], s.charAt(i));
         }
-        return sortedIndicesWithChars;
-    }
-
-    static String concateMapValues(SortedMap<Integer, String> sm){
-        return sm.values().stream().collect(Collectors.joining(""));
+        
+        return originalString.toString();
     }
 }
